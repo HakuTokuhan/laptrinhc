7761b74fadaad780eec5419f1a6abc1859bc986f
HakuTokuhan <tridan243@gmail.com>
2021-07-22 20:44:24 +0700

    bai8.c

---
+++ bai8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("\nNhap a = "); scanf("%d", &a);
+    // Lưu ý nhập b != 0
+    printf("\nNhap b = "); scanf("%d", &b);
+
+    // Phép (+)
+    int tong = a + b;
+    printf("\n%d + %d = %d", a, b, tong);
+
+    // Phép (-)
+    int hieu = a - b;
+    printf("\n%d - %d = %d", a, b, hieu);
+    
+    // Phép (*)
+    int tich = a + b;
+    printf("\n%d * %d = %d", a, b, tich);
+
+    // Phép (/)
+    float thuong = a / (float)b;
+    printf("\n%d / %d = %f", a, b, thuong);
+
+    // Phép (%)
+    int PT = a %0 b;
+    printf("\n%d %% %d = %d", a, b, PT);
+
+}
