b16172057088344575525e5d65ff9423a4c6d345
HakuTokuhan <tridan243@gmail.com>
2021-07-25 21:13:31 +0700

    bai11.c

---
+++ bai11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("\nNhap c: "); scanf("%c", &c);
+    printf("\nMa ASCII cua a la: %d", c);
+
+    /*
+    printf("\nGia tri cua c la: \'%c\'", c);
+
+    printf("\nc la ky tu a? %d", (a == 97));
+    int a = 97;
+    printf("\nGia tri cua a la: \'%c\'", (char)a);
+    */
+}
