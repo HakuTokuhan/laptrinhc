b96bda7e7853d9c51293db5a41a856ec4b4be989
HakuTokuhan <tridan243@gmail.com>
2021-07-27 22:23:48 +0700

    Sua lan 1

--- bai13-1.c
+++ bai13-1.c
@@ -5,13 +5,11 @@ int main()
     int a;
     printf("Nhap a: "); scanf("%d", &a);
     
-    if (a % 2 == 0) 
-    {
+    if (a % 2 == 0) {
         printf("\n%d la so chan", a);
     }
 
-    else 
-    {
+    else {
         printf("\n%d la so le", a);
     }
 }
