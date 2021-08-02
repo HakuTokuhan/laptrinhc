2c0a84373784e32a275fbbe88adb8fe7ce9aad2a
HakuTokuhan <tridan243@gmail.com>
2021-08-02 21:07:46 +0700

    Sua lan 1

--- bai21-1.c
+++ bai21-1.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-    int a, kt = 0;
+    int a;
+    bool kt = true;
     printf("Nhap so a: ");
     scanf("%d", &a);
 
     while (a > 9) {
         if (a % 2 != 0) {
-            kt ++;
+            kt = false;
             break;
         }
         a /= 10;
     }
 
-    if (kt == 0) {
+    if (kt == true) {
         printf("\nToan chu so chan");
     }
     else {
