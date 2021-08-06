b9454c8790c731fcbf76980193f5e1be42f303cd
HakuTokuhan <tridan243@gmail.com>
2021-08-06 22:14:49 +0700

    sua lan 1

--- bai25-1.c
+++ bai25-1.c
@@ -2,11 +2,15 @@
 
 int main()
 {
-    int a, b, bcnn;
+    int a, b, bcnn, i;
     printf("Nhap a: "); scanf("%d", &a);
     printf("Nhap b: "); scanf("%d", &b);
     int maxV = a * b;
-    for (int i = 1; i <= maxV; i++) {
+
+    i = a;
+    if (a < b) i = b;
+
+    for (i; i <= maxV; i++) {
         if (i % a == 0 && i % b == 0) {
             bcnn = i;
             break;
