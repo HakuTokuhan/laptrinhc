d152036b829e0282c57aafb92306b9fc57f20822
HakuTokuhan <tridan243@gmail.com>
2021-08-07 22:35:19 +0700

    bai26

---
+++ bai26/bai26-4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("7 ^ 3 = %f\n", pow(7, 3));
+    printf("4.73 ^ 12 = %f\n", pow(4.73, 12));
+    printf("32.01 ^ 1.54 = %f\n", pow(32.01, 1.54));
+}
---
+++ bai26/bai26-5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double param, result;
+    param = 1024.0;
+    result = sqrt(param);
+    printf("sqrt(%f) = %f\n", param, result);
+}
