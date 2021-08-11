69a1851918dbd6ba20fe564e4207728def9d07c7
HakuTokuhan <tridan243@gmail.com>
2021-08-11 22:49:25 +0700

    30-2.c

---
+++ bai30/30-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+static int dem = 0;
+
+int Lap(int n) {
+    dem++;
+    if (dem <= n) {
+        printf("Lap lan thu %d\n", dem);
+        Lap(n);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Nhap so lan lap: ");
+    scanf("%d", &n);
+    Lap(n);
+}
