e5d0af837a8a75ea80903769671cc9ffae7b73e0
HakuTokuhan <tridan243@gmail.com>
2021-08-12 20:32:37 +0700

    30-3.c

---
+++ bai30/30-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int Dem(int n) {
+    if (n == 0) return 0;
+    return 1 + Dem(n / 10);
+}
+
+int main()
+{
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    printf("\nSo luong chu so cua %d la: %d", n, Dem(n));
+}
