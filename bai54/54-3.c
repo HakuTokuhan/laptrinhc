024d4a3410ee4ecff8706c1ae07e0af840741997
HakuTokuhan <tridan243@gmail.com>
2021-09-07 22:55:46 +0700

    sua bai 54, 55; push bai 55

--- bai54/54-3.c
+++ bai54/54-3.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-void NhapMaTran(int a[][100], int n, int m) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++) {
+void NhapMaTran(int a[][100], int m, int n) {
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++) {
             printf("A[%d][%d] = ", i, j);
             scanf("%d", &a[i][j]);
         }
 }
 
-void XuatMaTran(int a[][100], int n, int m) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++)
+void XuatMaTran(int a[][100], int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
             printf("%d\t", a[i][j]);
         printf("\n");
     }
 }
 
-void ChuyenVi(int a[][100], int n, int m, int b[][100]) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
+void ChuyenVi(int a[][100], int m, int n, int b[][100]) {
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
             b[j][i] = a[i][j];
 }
 
@@ -26,14 +26,14 @@ int main()
 {
     int a[100][100], b[100][100];
     int m, n;
-    printf("Nhap so hang n: "); scanf("%d", &n);
-    printf("Nhap so cot m: "); scanf("%d", &m);
+    printf("Nhap so hang m: "); scanf("%d", &m);
+    printf("Nhap so cot n: "); scanf("%d", &n);
     printf("Nhap vao ma tran: \n");
-    NhapMaTran(a, n, m);
-    XuatMaTran(a, n, m);
+    NhapMaTran(a, m, n);
+    XuatMaTran(a, m, n);
 
-    ChuyenVi(a, n, m, b);
+    ChuyenVi(a, m, n, b);
 
     printf("\nMang sau khi doi hang thanh cot, cot thanh hang la: \n");
-    XuatMaTran(b, m, n);
+    XuatMaTran(b, n, m);
 }
--- bai55/55-4.c
+++ bai55/55-4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 
-const int MAX = 50;
+const int MAX = 100;
 
 void NhapMaTran(int a[][MAX], int m, int n) {
     for (int i = 0; i < m; i++)
@@ -19,36 +19,35 @@ void XuatMaTran(int a[][MAX], int m, int n) {
     }
 }
 
-int TinhTong(int a[][MAX], int m, int n) {
-    int s = 0;
-    for (int i = 0; i < m; i += 2)
+int PhanTuLonNhat(int a[][MAX], int m, int n) {
+    int max = a[0][0];
+    for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
-            s += a[i][j];
-    return s;
+            if (max < a[i][j])
+                max = a[i][j];
+    return max;
 }
 
-int TimMax(int a[][MAX], int m, int n) {
-    int max = a[0][0];
-    for (int i = 1; i < m; i++)
-        if (a[i][0] > max)
-            max = a[i][0];
-    return max;
+int PhanTuNhoNhat(int a[][MAX], int m, int n) {
+    int min = a[0][0];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            if (min > a[i][j])
+                min = a[i][j];
+    return min;
 }
 
 int main()
 {
     int a[MAX][MAX];
-    int  m, n;
+    int m, n;
     printf("Nhap so hang m: "); scanf("%d", &m);
     printf("Nhap so cot n: "); scanf("%d", &n);
     printf("Nhap vao ma tran:\n");
     NhapMaTran(a, m, n);
     printf("Ma tran vua nhap la: \n");
     XuatMaTran(a, m, n);
-
-    printf("\nTong pt tren hang chan cua mang la: %5d", TinhTong(a, m, n));
-    
-    printf("\nGia tri max tren cot 1 cua mang la %5d", TimMax(a, m, n));
-
-    getch();
+    printf("\nPhan tu lon nhat la %d", PhanTuLonNhat(a, m, n));
+    printf("\nPhan tu nho nhat la %d", PhanTuNhoNhat(a, m, n));
+    return 0;
 }
