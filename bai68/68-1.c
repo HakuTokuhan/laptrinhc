0254cfcf375ed587a440fb4ba30bc708a9f41a1d
HakuTokuhan <tridan243@gmail.com>
2021-10-02 22:38:46 +0700

    push bai68

---
+++ bai68/68-1.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+struct SingVien {
+    int maSV;
+    char ho[20];
+    char ten[20];
+    bool gioiTinh;
+    char queQuan[100];
+};
+
+int main()
+{
+    SinhVien sv1, sv2;
+    struct SinhVien sv3, sv4;
+    struct SinhVien sv[100];
+}
